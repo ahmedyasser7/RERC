d0e7684ae376243f725bc3499d0c0f5e56b990cc
Ahmed Yasser <ahmedyasser7714@gmail.com>
2023-12-24 00:01:36 +0200

    Adding elements into database

--- Database.py
+++ Database.py
@@ -1,31 +1,30 @@
 import sqlite3
 
 
-class Database():
+class Prop():
     def __init__(self, db):
-
         self.con = sqlite3.connect(db)
         self.cur = self.con.cursor()
 
         self.cur.execute(
-            "CREATE TABLE if not exists properties(id Integer Primary key autoincrement, Estate text, Area integer, Date text, Price integer, Status text, Rooms integer, Location text")
+            "CREATE TABLE if not exists properties(id INTEGER PRIMARY KEY AUTOINCREMENT, Estate TEXT, Area INTEGER, Date TEXT, Price INTEGER, Status TEXT, Rooms INTEGER, Location TEXT)")
         self.con.commit()
 
     def insert(self, Estate, Area, Date, Price, Status, Rooms, Location):
-        self.cur.execute("insert into student values (NULL,?,?,?,?,?,?,?)",
+        self.cur.execute("INSERT INTO properties VALUES (NULL,?,?,?,?,?,?,?)",
                          (Estate, Area, Date, Price, Status, Rooms, Location))
         self.con.commit()
 
     def fetch(self):
-        self.cur.execute("SELECT * FROM student")
+        self.cur.execute("SELECT * FROM properties")
         rows = self.cur.fetchall()
         return rows
 
     def remove(self, ID):
-        self.cur.execute("delete from student where id=?", (ID,))
+        self.cur.execute("DELETE FROM properties WHERE id=?", (ID,))
         self.con.commit()
 
     def update(self, ID, Estate, Area, Date, Price, Status, Rooms, Location):
-        self.cur.execute("update student set name=?,gpa=?,email=?,section=?,level=?,address=? where id=?",
+        self.cur.execute("UPDATE properties SET Estate=?, Area=?, Date=?, Price=?, Status=?, Rooms=?, Location=? WHERE id=?",
                          (Estate, Area, Date, Price, Status, Rooms, Location, ID))
         self.con.commit()
